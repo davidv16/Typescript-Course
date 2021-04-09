0ae4c7a78c8a30864a8b43fdb6e1dd273a1905fa
davidv16 <dabbithekking@gmail.com>
2021-04-09 09:42:14 +0000

    Section 15 complete

---
+++ Section 15/232. More on Decorators/decorators.ts
@@ -0,0 +1,45 @@
+@classDecorator
+class Boat {
+  @testDecorator
+  color: string = 'red';
+
+  @testDecorator
+  get formattedColor(): string {
+    return `This boats color is ${this.color}`;
+  }
+
+  @logError('Oops, boat was sunk')
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    throw new Error();
+    console.log('swish');
+  }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(target);
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method;
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
+}
